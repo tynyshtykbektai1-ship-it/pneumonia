52254913fc9b264586d599e1f5f8b4b10f6579d8
tynyshtykb <tynyshtykbektai1@gmail.com>
2026-01-07 15:39:51 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,11 +15,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/justicon.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/justicon.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/justicon.png",
   },
 }
 
